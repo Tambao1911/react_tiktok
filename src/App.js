6a6e3c3acf6dca1c3992e56fd886bd879f7052c9
phamvantam <phamvantam1911@gmail.com>
2023-09-07 12:26:34 +0700

    apiServices -> services, src/components/Layout ->src/layouts, routes config, Tạo file env, đưa cấu hình baseUrl ra .env.development

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { Fragment } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { publicRoutes } from '~/routes';
-import { DefaultLayout } from '~/component/Layout';
+import DefaultLayout from '~/layouts';
 
 function App() {
     return (
@@ -10,12 +10,12 @@ function App() {
                 <Routes>
                     {publicRoutes.map((route, index) => {
                         const Page = route.component;
-                        let Layout = DefaultLayout
+                        let Layout = DefaultLayout;
 
                         if (route.layout) {
-                            Layout = route.layout
+                            Layout = route.layout;
                         } else if (route.layout === null) {
-                            Layout = Fragment
+                            Layout = Fragment;
                         }
 
                         return (
